b8544dcb868ba495c3880a999e876ae82b8c893e
aakashnarayan <42978492+aakashnarayan@users.noreply.github.com>
2018-10-02 14:30:11 -0400

    Add files via upload

---
+++ src/RectCalc.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * @author Aakash Narayan
+ */
+
+import java.util.Scanner;
+
+public class RectCalc 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the length of the rectangle: ");
+		double length = in.nextDouble();
+		in.nextLine();
+		System.out.print("Enter the width of the rectangle: ");
+		double width = in.nextDouble();
+		in.nextLine();
+		
+	}
+
+}
